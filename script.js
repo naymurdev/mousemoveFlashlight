24afd17815ea4de812d751d839a4b6ddc80fc758
ivan <ivanvaneck2000@gmail.com>
2023-09-11 01:43:16 +0300

    flashlight mode, fancy cursor, cursor position

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const cursor = document.getElementById("cursor");
+const FLModeCheckbox = document.getElementById("fl-mode");
 const element = document.getElementById("sample-text");
 
-document.addEventListener('mousemove', e => {
+document.addEventListener("mousemove", (e) => {
   cursor.style.top = e.pageY + "px";
   cursor.style.left = e.pageX + "px";
+});
+
+FLModeCheckbox.addEventListener("click", (_) => {
+  document.body.dataset.flmode = FLModeCheckbox.checked;
 });
